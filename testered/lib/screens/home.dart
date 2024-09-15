beeed12ee129551616cf19fb3632f1d6543ba9db
Andrew <bayledgeacademy@icloud.com>
2024-09-15 17:21:33 -0500

    Did 2 screen clicker iterator tutorial. WILL DELETE CODE FOR THIS TO DO NEXT TUTORIAL
    See this commit for the code for the 2 screen switcher.

---
+++ testered/lib/screens/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:testered/controllers/counterController.dart';
+import 'package:testered/screens/other.dart';
+
+class HomeScreen extends StatelessWidget{
+  final CounterController counterController = Get.put(CounterController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(()=>Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("Clicks: ${counterController.counter.value}")),
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+              child: ElevatedButton(onPressed: (){
+                Get.to(OtherScreen());
+              }, child: Text("Open Other Screen"))
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(onPressed: (){counterController.increment();},
+      child: Icon(Icons.add),
+      ),
+
+    );
+  }
+}
