e5b109ef5d89a7e6f8fedafc37b004fe52790e94
fullmoon44 <137942546+fullmoon44@users.noreply.github.com>
2024-09-17 04:28:33 -0500

    Add files via upload
    Worked some more on the home page- it's not a lot but the font stuff was giving me a lotta trouble for no reason.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'login.dart'; // Import the LoginPage class
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,89 +48,63 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
+        toolbarHeight: 80,
         title: Image.asset('logo.webp', height: 50, width: 50,),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.account_circle, size: 50,), onPressed:(){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-          }),    
-        ],
-      ),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        flexibleSpace: Padding( //Creates the banner stuff
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[ 
+                  Container(
+                  decoration: BoxDecoration( //Search bar thing
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: 150,
+                  height: 30,
+                  child:Align( //The user icon part
+                    alignment: Alignment.topRight,
+                    child:
+                    IconButton(icon: Icon(Icons.search, size:20), onPressed:(){},),
+                  ),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children:<Widget>[
+                      IconButton(icon: Icon(Icons.account_circle, size: 40,), onPressed:(){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+                    }),
+                    Text('Sign in.', style: TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold),),
+                    ]
+                  ),    
+              ], 
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          ),
+        ),
+
+      body: 
+      Padding( //Actual site stuff
+        padding: const EdgeInsets.all(0.0),  
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset('mainPageBanner.jpg', fit: BoxFit.fitWidth, height: 690,),           
+              Text('Recent Events', style: GoogleFonts.bebasNeue(fontWeight: FontWeight.bold, fontSize: 30,),),
+              Text('$_counter',style: Theme.of(context).textTheme.headlineMedium,),
           ],
         ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
 }
-
-/*class Login extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Stack(
-          children: <Widget>[ 
-          Image.asset(
-            'loginBG.jpg',
-             fit: BoxFit.cover,
-             height: double.infinity,
-             width: double.infinity,
-             ),
-          Center(
-            child: Opacity( 
-              opacity: 0.7,
-                child: Container(
-                  width: 400,
-                  height: 500,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      width: 5.0,
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text('Sign In', style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold)),
-                        Text('which type of account do you want to sign into?', style: TextStyle(color: Colors.black, fontSize: 20), textAlign: TextAlign.center,),
-                        ElevatedButton(onPressed: onPressed, child: Text('VOLUNTEER', style: TextStyle(color: Colors.white),)),
-                        ElevatedButton(onPressed: onPressed, child: Text('HOST', style: TextStyle(color: Colors.white),)),
-                      ],
-                    ),
-                ),
-                )
-            )
-          )
-          ], //Widgets
-        ), //Column
-      ),
-    );
-  }
-} */
-
