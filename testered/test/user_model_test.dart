8e35008e96b85ca4b8047a21dc5e0881809940d7
Juan Son <91796609+son-juan@users.noreply.github.com>
2024-11-20 16:22:01 -0600

    Update user_model_test.dart

--- testered/test/user_model_test.dart
+++ testered/test/user_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:testered/lib/models/user_model.dart';
+import 'package:testered/models/user_model.dart';
+
 
 void main() {
   group('User Model Tests', () {
