c6c560ead6cdb4adcafbd46f54061c92d9b023c0
Juan Son <91796609+son-juan@users.noreply.github.com>
2024-11-21 23:52:57 -0600

    Update profile_screen_admin.dart

--- testered/lib/screens/profile_screen_admin.dart
+++ testered/lib/screens/profile_screen_admin.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; // For date formatting
 import 'package:provider/provider.dart';
-import 'package:universal_html/html.dart';
 import '../services/custom_nav_bar.dart';
 import '../models/user_model.dart';
 import '../models/event_model.dart'; // Assuming you have an Event model
